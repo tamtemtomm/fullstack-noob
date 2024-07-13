e2d8eec404fb7d50ab61503d629ac2d61d850321
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-07-13 22:06:57 +0700

    Learn Authenticate with Firebase

---
+++ firebase-auth/src/firebase.js
@@ -0,0 +1,9 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth"
+import { firebaseConfig } from "./firebase-config";
+
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app)
