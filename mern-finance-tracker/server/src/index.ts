94e18b43f613699f058164e18593549a3a6b1987
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2024-07-29 15:39:14 +0700

    Add update record and delete functionalities

--- mern-finance-tracker/server/src/index.ts
+++ mern-finance-tracker/server/src/index.ts
@@ -2,14 +2,15 @@ import express, { Express } from "express";
 import mongoose from "mongoose";
 import FinancialRecordRouter from "./routes/financial-record";
 import cors from "cors";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 app.use(express.json());
 app.use(cors());
 
-const mongoURL: string =
-  "mongodb+srv://tamtemtom235:Qb5Z8YzUYQd5Y1Oq@cluster0.xxhq2vu.mongodb.net/";
+const mongoURL: string = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.4v2dxmn.mongodb.net/`;
 
 mongoose
   .connect(mongoURL)
--- mern-finance-tracker/server/src/routes/financial-record.ts
+++ mern-finance-tracker/server/src/routes/financial-record.ts
@@ -3,21 +3,18 @@ import FinancialRecordModel from "../shema/financial-record";
 
 const router = express.Router();
 
-router.get(
-  "/getAllByUserId:userId",
-  async (req: Request, res: Response) => {
-    try {
-      const userId = req.params.userId;
-      const records = await FinancialRecordModel.find({ userId });
-      if (records.length === 0) {
-        return res.status(404).send("No records found for the user.");
-      }
-      res.status(200).send(records);
-    } catch (err) {
-      res.status(500).send(err);
+router.get("/getAllByUserID/:userId", async (req: Request, res: Response) => {
+  try {
+    const userId = req.params.userId;
+    const records = await FinancialRecordModel.find({ userId: userId });
+    if (records.length === 0) {
+      return res.status(404).send("No records found for the user.");
     }
+    res.status(200).send(records);
+  } catch (err) {
+    res.status(500).send(err);
   }
-);
+});
 
 router.post("/", async (req: Request, res: Response) => {
   try {
