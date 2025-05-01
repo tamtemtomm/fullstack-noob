4702505ec3aa4c29fdfb8306b81b23e6f68b2d32
Tamtemtom020604 <timo.widyanvolta-2021@ftmm.unair.ac.id>
2025-05-01 21:38:27 +0700

    Update

--- firebase-realtime-chat/src/firebase-config.js
+++
@@ -1,9 +0,0 @@
-// Your web app's Firebase configuration
-export const firebaseConfig = {
-    apiKey: "AIzaSyDQo7eWPZp9Fs0iD5GCTZX4AbS__2LWAo0",
-    authDomain: "fir-realtime-chat-7fc32.firebaseapp.com",
-    projectId: "fir-realtime-chat-7fc32",
-    storageBucket: "fir-realtime-chat-7fc32.appspot.com",
-    messagingSenderId: "529350884248",
-    appId: "1:529350884248:web:52c788e5eec4dfa63789ed"
-  };
